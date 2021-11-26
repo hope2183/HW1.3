e27f2814b0f18968b12796de2d6d989a3dd8c001
hope2183@mail.ru <hope2183@mail.ru>
2021-11-26 16:20:56 +1100

    BranchCommit 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,79 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+        // TASK 1
+        int clientOS = 0; //0 — iOS, 1 — Android
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для ANDROID по ссылке");
+        } else {
+            System.out.println("ВНИМАНИЕ! Неверное наименование ОС!");
+        }
+
+        //TASK 2
+        int clientDeviceYear = 2010;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для ANDROID по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015){
+            System.out.println("Установите облегченную версию приложения для ANDROID по ссылке");
+        } else {
+            System.out.println("ВНИМАНИЕ! Неверные данные");
+        }
+
+        //TASK 3
+        int year = 1200;
+        if (year % 4==0 && year % 100 !=0 || year % 400 == 0){
+            System.out.println(year + " год является високосным.");
+        } else {
+            System.out.println(year + " год не является високосным.");
+        }
+//
+        //TASK 4
+        int deliveryDistanceKm = 100;
+
+            if (deliveryDistanceKm<20){
+            System.out.println("На доставку потребуется 1 день.");
+            }else if (deliveryDistanceKm>=20&&deliveryDistanceKm<60){
+                System.out.println("На доставку потребуется 2 дня.");
+            }else if (deliveryDistanceKm>=60&&deliveryDistanceKm<100){
+                System.out.println("На доставку потребуется 3 дня.");
+            }else {
+                System.out.println("Доставить не сможем.");
+            }
+
+
+        //TASK 5
+
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("ЗИМА");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("ВЕСНА");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("ЛЕТО");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("ОСЕНЬ");
+                break;
+            default:
+                System.out.println("ТАКОГО МЕСЯЦА НЕ СУЩЕСТВУЕТ");
+        }
+
+    }}
+
